ae8ebc19f0a6749eaf21815847b6e794fcfe5bb6
marcrine-geek <marcrinemm@gmail.com>
2024-09-19 15:51:38 +0300

    remove any existing installations

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,10 @@
 # navigate to app folder
 cd /app
 
+# Remove any existing node and npm installations
+apt-get remove --purge -y nodejs npm
+apt-get autoremove -y
+
 # install node and npm
 apt-get install curl
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
